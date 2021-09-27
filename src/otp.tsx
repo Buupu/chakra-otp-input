283faaed31a557f9e84b87f8f5a952680e6137fc
Buupu <sammyfattah@gmail.com>
2021-09-27 17:45:39 +0100

    Inputs will now focus next input after user enters character

--- src/otp.tsx
+++ src/otp.tsx
@@ -1,20 +1,30 @@
 import { HStack, Input } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const OTPInput = ({ noInputs }: OTPProps) => {
+  const [activeInput, setActiveInput] = useState<number>(0);
+  const inputRefs = useRef<HTMLInputElement[]>([]);
+
+  useEffect(() => {
+    inputRefs.current[activeInput]?.focus();
+  }, [activeInput]);
+
   const getInputs = () => {
     const inputs = [];
 
     for (let i = 0; i < noInputs; i++) {
       inputs.push(
         <Input
-          maxlength={1}
+          key={`otp-input-${i}`}
+          maxLength={1}
           w={12}
           h={12}
           p={0}
           textAlign="center"
           boxShadow="sm"
           bg="white"
+          onInput={() => setActiveInput(i + 1)}
+          ref={(ref: HTMLInputElement) => (inputRefs.current[i] = ref)}
         />
       );
     }
