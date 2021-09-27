826caf5009fed3d1df5f0eae5e140e33cc66dc2f
Buupu <sammyfattah@gmail.com>
2021-09-27 16:37:14 +0100

    noInputs prop added to OTPInput component - which remembers the number of inputs

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Box, ChakraProvider, theme } from "@chakra-ui/react";
 import * as React from "react";
 import { OTPInput } from "../../";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <OTPInput />
+    <Box width="300px" h="100vh">
+      <OTPInput noInputs={5} />
+    </Box>
   </ChakraProvider>
 );
--- src/otp.tsx
+++ src/otp.tsx
@@ -1,6 +1,19 @@
-import { Stack, Heading } from "@chakra-ui/react";
+import { HStack, Input } from "@chakra-ui/react";
 import React from "react";
 
-export const OTPInput = () => {
-  return <Stack></Stack>;
+export const OTPInput = ({ noInputs }: OTPProps) => {
+  const getInputs = () => {
+    const inputs = [];
+
+    for (let i = 0; i < noInputs; i++) {
+      inputs.push(<Input maxlength={1} />);
+    }
+
+    return inputs;
+  };
+  return <HStack>{getInputs()}</HStack>;
 };
+
+interface OTPProps {
+  noInputs: number;
+}
