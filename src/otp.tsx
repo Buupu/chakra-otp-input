2d8eef6d7b46e6e7a66fa70a84559a49dbe1ac11
Buupu <sammyfattah@gmail.com>
2021-09-27 19:55:36 +0100

    Added keyboard funcationalities

--- src/otp.tsx
+++ src/otp.tsx
@@ -1,13 +1,67 @@
 import { HStack, Input } from "@chakra-ui/react";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, KeyboardEvent } from "react";
 
 export const OTPInput = ({ noInputs }: OTPProps) => {
+  const [input, setInput] = useState<string[]>([]);
   const [activeInput, setActiveInput] = useState<number>(0);
   const inputRefs = useRef<HTMLInputElement[]>([]);
 
   useEffect(() => {
     inputRefs.current[activeInput]?.focus();
   }, [activeInput]);
+
+  const moveToNextInput = () => {
+    if (activeInput !== noInputs - 1) {
+      setActiveInput(activeInput + 1);
+    }
+  };
+
+  const moveToPrevInput = () => {
+    if (activeInput !== 0) {
+      setActiveInput(activeInput - 1);
+    }
+  };
+
+  const clearCurrentFocusValue = () => {
+    const tempInput = [...input];
+    tempInput[activeInput] = "";
+    setInput(tempInput);
+  };
+
+  const setCurrentFocusValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const tempInput = [...input];
+    tempInput[activeInput] = e.target.value;
+    setInput(tempInput);
+  };
+
+  const setActiveInputOnFocus = (focusIndex: number) => {
+    if (activeInput !== focusIndex) {
+      setActiveInput(focusIndex);
+    }
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch (e.key) {
+      case "Backspace":
+        clearCurrentFocusValue();
+        moveToPrevInput();
+        e.preventDefault();
+        break;
+      case "Space":
+      case "Spacebar":
+      case " ":
+        e.preventDefault();
+        break;
+      case "ArrowLeft":
+        e.preventDefault();
+        moveToPrevInput();
+        break;
+      case "ArrowRight":
+        e.preventDefault();
+        moveToNextInput();
+        break;
+    }
+  };
 
   const getInputs = () => {
     const inputs = [];
@@ -23,7 +77,15 @@ export const OTPInput = ({ noInputs }: OTPProps) => {
           textAlign="center"
           boxShadow="sm"
           bg="white"
-          onInput={() => setActiveInput(i + 1)}
+          onInput={moveToNextInput}
+          onKeyDown={handleKeyDown}
+          value={input[i] || ""}
+          onChange={(e) => {
+            setCurrentFocusValue(e);
+          }}
+          onFocus={() => {
+            setActiveInputOnFocus(i);
+          }}
           ref={(ref: HTMLInputElement) => (inputRefs.current[i] = ref)}
         />
       );
