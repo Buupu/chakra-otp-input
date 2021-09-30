2e6ec98e9729f3c72724b29df5e19491509806e7
buupu <sammyfattah@gmail.com>
2021-09-30 21:26:56 +0100

    Added unit tests for isPrivate and isNumeric props

--- test/otp.test.tsx
+++ test/otp.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { OTPInput } from "../";
 
@@ -49,4 +50,48 @@ test("Checks inputs are set to password type", () => {
   otpInputFields.forEach((input) =>
     expect(input).toHaveProperty("type", "password"),
   );
+});
+
+test("Checks inputs aren't set to password type", () => {
+  render(<OTPInput onChange={() => {}} noInputs={3} isPrivate={false} />);
+
+  const otpInputFields = screen.getAllByTestId("chakra-otp-input-field");
+
+  otpInputFields.forEach((input) =>
+    expect(input).toHaveProperty("type", "text"),
+  );
+});
+
+test("Checks inputs are set to password type", () => {
+  render(<OTPInput onChange={() => {}} noInputs={3} isPrivate={true} />);
+
+  const otpInputFields = screen.getAllByTestId("chakra-otp-input-field");
+
+  otpInputFields.forEach((input) =>
+    expect(input).toHaveProperty("type", "password"),
+  );
+});
+
+test("Checks isNumeric prop blocks user entering non number characters", () => {
+  render(<OTPInput onChange={() => {}} noInputs={3} isNumeric={true} />);
+
+  const otpInputFields: HTMLInputElement[] = screen.getAllByTestId(
+    "chakra-otp-input-field",
+  );
+
+  userEvent.type(otpInputFields[0], "a");
+
+  expect(otpInputFields[0].value).toEqual("");
+});
+
+test("Checks isNumeric prop still allows user to enter numbers", () => {
+  render(<OTPInput onChange={() => {}} noInputs={3} isNumeric={true} />);
+
+  const otpInputFields: HTMLInputElement[] = screen.getAllByTestId(
+    "chakra-otp-input-field",
+  );
+
+  userEvent.type(otpInputFields[0], "1");
+
+  expect(otpInputFields[0].value).toEqual("1");
 });
